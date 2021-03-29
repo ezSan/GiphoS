7b3e013d392dfbacf8924339ccb7e28adce44f2b
Parkka <ezequiel.sanchez.7391@gmail.com>
2021-03-29 19:52:13 -0300

    Add_trending

--- scripts/trendings.js
+++ scripts/trendings.js
@@ -1,29 +1,38 @@
-
- let trending = () => {
+let trending = () => {
     
     let traer = ()=> {
-        fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=3&offset=1&rating=g`)
+        fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=15&offset=1&rating=g`)
         .then(res => res.json())
-        .then((gif)=> {
+        .then((gif)=> {            
+            console.log(gif.data[1].images.downsized.url)
+            for(var i = 0; i <= 14 ; i++){
             // url Imagen
-            let tdnGif = gif.data[1].images.downsized.url 
+            let tdnGif = gif.data[i].images.downsized.url 
             // Accedemos al nodo html
             let trendigContent = document.getElementById(`trending-ctn`); 
             // crear elemento imagen
             let createGif = document.createElement(`img`);
+            createGif.classList.add("gipho")
             // asignar src imagen  = url.imagen
             createGif.src= tdnGif;
             //asignar hijo a nodo html
             trendigContent.appendChild(createGif);
-        })
-       }
-   traer();
-    }
+            }
+
+        }
+            ) 
+            } 
+  traer()
            
-trending();
+
+            }
+ 
+
+ trending();
      
 
     // accedo al objeto, recorro sus propiedades y accedo a una en particular
     //.then(gif=> console.log(gif.data[0].url))  ===>>> accedo a la URL del giff
     //.then(gif=> console.log(gif.data[0].images.downsized_medium.url)) 
+    //.then(gif=> console.log(gif.data))  --->Array with gif
     
