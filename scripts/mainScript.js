873a6a7f0cdeb56c1cde311e9f25114081cdaf06
ezSan <ezequiel.sanchez.7391@gmail.com>
2021-06-18 15:24:08 -0300

    Re haciendo fcn tools & dataGif desde array result

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -10,15 +10,44 @@ let userSearchinput = document.getElementById(`search`);
 
 let resultadosDeBusqueda = [];
 
+
 // Array que almacena resultados favoritos
 
-let urlFavoritos = [];
+const urlFavoritos = [];
 
 
 //objeto gif 
 
-function Gif (url, title, author) {
+/* function newGif (url, title, author ,) {
     this.url = url ; 
     this.title = title; 
     this.author = author; 
-}
+} */
+
+
+
+
+// showresult () -- > 
+
+
+
+
+
+
+      
+/* let cajaBusqueda = document.getElementById('out');
+let boxWithGif = document.createElement('div');
+boxWithGif.id = 'boxWithGif';
+let src = content.data[i].images.downsized.url;
+let title = content.data[i].title;
+let author = content.data[i].user; 
+let searchResult = new Gif (src, title, author);
+/*  boxWithGif.appendChild(searchResult); */
+/* let img = document.createElement('img');
+img.id = 'gipho';
+img.classList.add = 'result';
+img.src
+boxWithGif.appendChild(img);
+cajaBusqueda.insertAdjacentElement('afterbegin', boxWithGif);
+document.querySelector(`#search`).value = ' ';
+ */ 
--- scripts/searchbtn.js
+++ scripts/searchbtn.js
@@ -5,7 +5,7 @@ function init() {
     document.getElementById("btnSearch").addEventListener("click", ev => {
         ev.preventDefault(); // dont refresh
 
-        let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=36&q=`;
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=24&q=`;
         let str = document.getElementById(`search`).value;
 
         url = url.concat(str);
@@ -14,14 +14,16 @@ function init() {
         fetch(url)
             .then(response => response.json())
             .then(content => {  
-               
+
+                intoArray(content);                
                 removeResults();
-                showResults(content);
+                showResults();
                 addTitle(str);
-                btnVerMas();
-                createToolsResult();
-                titleAndAuthorResult(content);
-                intoArray(content);
+                btnVerMas();   
+                toolsBox()             
+                
+                /* titleAndAuthorResult(content);  */
+                
 
                 
             })
@@ -32,92 +34,97 @@ function init() {
     });
 }
 
-//Capturar data del fetch, iterar los resultados e imprimir en pantalla
-
-
-let showResults = (content) => {
-
-    for (i = 0; i < 24; i++) {
-        let fig = document.createElement("div");
-        fig.id = 'resultCtn';
-        /* fig.classList.add('resultCtn') */
-        let img = document.createElement("img");
-        img.classList.add(`result`);
-        img.id = `gipho`;
-        img.src = content.data[i].images.downsized.url;
-        img.title = content.data[i].title;
-        img.author = content.data[i].user;        
-        fig.appendChild(img);        
-        let out = document.querySelector("#out");
-        out.insertAdjacentElement("afterbegin", fig);
-        document.querySelector(`#search`).value = ' ';
-        if (i > 11) {
-            fig.classList.add('hidden');
-        }
-    }
-}
-
-
-
-
 //Almacenar los resultados de busqueda en un array provisorio
 
 let intoArray = (content) => {
     if (resultadosDeBusqueda.length == 0) {
-        resultadosDeBusqueda.push(content);
+        resultadosDeBusqueda.push(content.data);
     } else {
         resultadosDeBusqueda.pop();
-        resultadosDeBusqueda.push(content);
+        resultadosDeBusqueda.push(content.data);
     }
-    console.log(resultadosDeBusqueda[0].data);
+    console.log(resultadosDeBusqueda[0]);
 }
 
 
+//Capturar data del fetch, iterar los resultados e imprimir en pantalla
+
+
+let showResults = () => {
+
+    let arrayResults = resultadosDeBusqueda[0];
+
+    for (i = 0;  i < arrayResults.length; i++){
+
+        let cajaGif = document.createElement('div');
+        cajaGif.classList.add('cajaGif');
+        let gif = document.createElement('img');
+        gif.classList.add('result');
+        let url = arrayResults[i].images.downsized.url;
+        let title = arrayResults[i].title;        
+        let author = arrayResults[i].username;        
+        gif.src = url;
+        cajaGif.appendChild(gif);
+        let out = document.getElementById('out');
+        out.insertAdjacentElement("afterbegin", cajaGif);
+        if (i > 11) {
+            cajaGif.classList.add('cajaGif');
+            cajaGif.classList.add('hidden');
+        document.querySelector(`#search`).value = ' '
+        
+        } 
+    }
+   
+}
 
 //Insertar caja de herramientas en c/gif resultante de busqueda
 
-let createToolsResult = () => {
 
-    let resultCtn = document.querySelectorAll('#resultCtn');
-
-    for (i = 0; i < resultCtn.length; i++) {
-        let cajitaTools = document.createElement('div');
-        cajitaTools.classList.add('cajitaTools');
-        cajitaTools.id = 'cajitaTools';
-        resultCtn[i].appendChild(cajitaTools);
-        let favAdd = document.createElement('img');
-        favAdd.src = "./assets/icon-fav.svg";
-        cajitaTools.appendChild(favAdd);
-        favAdd.classList.add('inactive');
-        let url_para_favAdd = resultCtn[i].firstChild.src;        
-        let title_para_favAdd = resultCtn[i].firstChild.title;        
-        let author_para_favAdd = resultCtn[i].firstChild.user;        
-        let dwlAdd = document.createElement('img');
-        dwlAdd.src = "./assets/icon-download.svg";
-        cajitaTools.appendChild(dwlAdd);
-        let mViewAdd = document.createElement('img');
-        mViewAdd.src = "./assets/icon-max-normal.svg";
-        cajitaTools.appendChild(mViewAdd);
-        favAdd.addEventListener('click', function () {
-            if (favAdd.classList.contains('inactive')) {
-                favAdd.classList.remove('inactive')
-                favAdd.classList.add('active')
-                favAdd.src = "./assets/icon-fav-active.svg";
-                let obj = new Gif (url_para_favAdd, title_para_favAdd, author_para_favAdd);
-                console.log(obj);
-                urlFavoritos.push(obj);
-                console.log(urlFavoritos);
-                console.log(urlFavoritos.indexOf(obj));
-            } else if (favAdd.classList.contains('active')) {
-                favAdd.classList.remove('active');
-                favAdd.classList.add('inactive');
-                favAdd.src = "./assets/icon-fav.svg";
-                              
-            }
-        })
-    };
-    
+let toolsBox = () => {
+    let box = document.querySelectorAll('#cajaGif');
+    box.id = 'napoPete';
 }
+
+// let createToolsResult = () => {        
+    
+//     for (i = 0; i < resultadosDeBusqueda[0].length; i++) {
+
+//             let cajitaTools = document.createElement('div');
+//             cajitaTools.classList.add('cajitaTools');  
+//             let cajaDeGif = document.getElementsByClassName('cajaGif');    
+//             cajaDeGif.appendChild(cajitaTools); 
+//             let favAdd = document.createElement('img');
+//             favAdd.src = "./assets/icon-fav.svg";
+//             cajitaTools.appendChild(favAdd);
+//             favAdd.classList.add('inactive');
+//             let url_para_favAdd = resultadosDeBusqueda[0][i].firstChild.src;        
+//             let title_para_favAdd = resultadosDeBusqueda[0][i].firstChild.title;        
+//             let author_para_favAdd = resultadosDeBusqueda[0][i].firstChild.user;        
+//             let dwlAdd = document.createElement('img');
+//             dwlAdd.src = "./assets/icon-download.svg";
+//             cajitaTools.appendChild(dwlAdd);
+//             let mViewAdd = document.createElement('img');
+//             mViewAdd.src = "./assets/icon-max-normal.svg";
+//             cajitaTools.appendChild(mViewAdd);
+//             favAdd.addEventListener('click', function () {
+//                 if (favAdd.classList.contains('inactive')) {
+//                     favAdd.classList.remove('inactive')
+//                     favAdd.classList.add('active')
+//                     favAdd.src = "./assets/icon-fav-active.svg";
+//                     let dataFaveado = [url_para_favAdd , title_para_favAdd , author_para_favAdd];
+//                     urlFavoritos.push(dataFaveado);
+//                     console.log(urlFavoritos);
+//                 } else if (favAdd.classList.contains('active')) {
+//                     favAdd.classList.remove('active');
+//                     favAdd.classList.add('inactive');
+//                     favAdd.src = "./assets/icon-fav.svg";
+                                  
+//                 }
+//             })
+//         };
+        
+//     }
+
 
 //insertar gifData en cada caja 
 
@@ -199,40 +206,3 @@ let btnVerMas = () => {
 
 
 
-/* favAdd.addEventListener('click', function(){
-    if (localStorage.length ==0){
-
-    }else{
-
-
-
-
-      if ( favAdd.classList.contains('inactive')) {
-        let storageContent = localStorage.getItem('Favoritos');
-        ;
-        listaDeFavs.push(storageContent);
-        favAdd.classList.remove('inactive')
-        favAdd.classList.add('active')
-        favAdd.src="./assets/icon-fav-active.svg"
-        //AGREGAR AL LOCALSTORAGE
-        listaDeFavs.push(url_para_favAdd)
-inde
-        localStorage.setItem('Favoritos', listaDeFavs);
-
-    } else if ( favAdd.classList.contains('active')) {
-        favAdd.classList.remove('active');
-        favAdd.classList.add('inactive');
-
-
-
-        //ELIMINAR  DEL LOCALSTORAGE
-        console.log(lista_de_faveados)
-        if (listaDeFavs.includes(url_para_favAdd)){
-        listaDeFavs.splice(listaDeFavs.indexOf(url_para_favAdd),1 )
-        localStorage.setItem('Favoritos', listaDeFavs);
-        }
-    }
-
-}
-
-}) */
