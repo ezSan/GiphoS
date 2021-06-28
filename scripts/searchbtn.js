92cf176ca5109f2b47b4f2772a562fad6163e498
ezSan <ezequiel.sanchez.7391@gmail.com>
2021-06-28 17:36:31 -0300

    CleanCode: add tools & title+author recycled for more functions

--- scripts/searchbtn.js
+++ scripts/searchbtn.js
@@ -18,15 +18,9 @@ function init() {
                 intoArray(content);                
                 removeResults();
                 showResults(); 
-                createToolsBusqueda();               
                 addTitle(str);
                 btnVerMas();   
-                          
-                
-                /* titleAndAuthorResult(content);  */
-                
-
-                
+                             
             })
 
             .catch(err => {
@@ -48,7 +42,7 @@ let intoArray = (content) => {
 }
 
 
-//Capturar data del fetch, iterar los resultados e imprimir en pantalla
+//Capturar data del array de busqueda y mostrar en DOM
 
 
 let showResults = () => {
@@ -73,33 +67,31 @@ let showResults = () => {
         gifTitle.insertAdjacentElement("afterend", gifAuthor); 
         gifAuthor.classList.add('hidden'); 
         gif.src = url;
-        /* gif.title = title; 
-        gif.author = author; */
         cajaGif.appendChild(gif);
         let out = document.getElementById('out');
         out.insertAdjacentElement("afterbegin", cajaGif);
         if (i > 11) {
             cajaGif.classList.add('cajaGif');
             cajaGif.classList.add('hidden');
-        document.querySelector(`#search`).value = ' ';        
         } 
+
+        createTools(cajaGif); 
+        titleAndAuthor(title,author,cajaGif);  
+             
     }
+
+    document.querySelector(`#search`).value = ' ';        
    
 }
 
-//Insertar caja de herramientas en c/gif resultante de busqueda
+//Insertar caja de herramientas en cada caja que contenga gif
 
 
-let createToolsBusqueda = () => {  
-
-    let allBoxWithGif = document.querySelectorAll('.cajaGif');
-    
-
-    for (i=0; i <allBoxWithGif.length; i++) {        
-        
+let createTools = (cajaGif) => {  
+       
         let cajitaTools = document.createElement('div');
-        cajitaTools.classList.add('cajitaTools[i]'); 
-        allBoxWithGif[i].appendChild(cajitaTools);
+        cajitaTools.classList.add('cajitaTools'); 
+        cajaGif.appendChild(cajitaTools);
         let favAdd = document.createElement('img');
         favAdd.src = "./assets/icon-fav.svg";
         cajitaTools.appendChild(favAdd);
@@ -109,101 +101,37 @@ let createToolsBusqueda = () => {
         cajitaTools.appendChild(dwlAdd);
         let mViewAdd = document.createElement('img');
         mViewAdd.src = "./assets/icon-max-normal.svg";
-        cajitaTools.appendChild(mViewAdd); 
-        let thisUrl = allBoxWithGif[i].childNodes[2].id
-         
+        cajitaTools.appendChild(mViewAdd);                 
         favAdd.addEventListener('click', function () {
              if (favAdd.classList.contains('inactive')) {
                  favAdd.classList.remove('inactive')
                  favAdd.classList.add('active')
-                 favAdd.src = "./assets/icon-fav-active.svg"; 
-                 console.log(thisUrl);
-            //   let dataFaveado = [url_para_favAdd , title_para_favAdd , author_para_favAdd];
-            //   urlFavoritos.push(dataFaveado);
-            //   console.log(urlFavoritos);
+                 favAdd.src = "./assets/icon-fav-active.svg";           
              } else if (favAdd.classList.contains('active')) {
                  favAdd.classList.remove('active');
                  favAdd.classList.add('inactive');
                  favAdd.src = "./assets/icon-fav.svg";                             
              } 
          })
-        };
-        
-    }
+        };      
 
-    
+//insertar gifData en cada caja que contenga gif
 
-
-
-// let toolsBox = () => {
-//     let box = document.querySelectorAll('#ctnTools');
-//     let arrayResults = resultadosDeBusqueda[0];
-    
-//      for (i=0; i<box.length; i++){
-//         /* console.log(box[i]);  */
-//         let cajaTools = document.createElement('div');
-//         cajaTools.classList.add('toolsBox');
-//         box[i].appendChild(cajaTools);
-//         let favAdd = document.createElement('img');
-//         favAdd.src = "./assets/icon-fav.svg";
-//         cajaTools.appendChild(favAdd);
-//         favAdd.classList.add('inactive');       
-//         let dwlAdd = document.createElement('img');
-//         dwlAdd.src = "./assets/icon-download.svg";
-//         cajaTools.appendChild(dwlAdd);
-//         let mViewAdd = document.createElement('img');
-//         mViewAdd.src = "./assets/icon-max-normal.svg";
-//         cajaTools.appendChild(mViewAdd)
-//         favAdd.addEventListener('click', function () {
-//                  if (favAdd.classList.contains('inactive')) {
-//                      favAdd.classList.remove('inactive')
-//                      favAdd.classList.add('active')
-//                      favAdd.src = "./assets/icon-fav-active.svg";                       
-//                      let dataFaveado = [this.url, this.title];
-//                      urlFavoritos.push(dataFaveado);
-//                      console.log(urlFavoritos);
-//                  } else if (favAdd.classList.contains('active')) {
-//                      favAdd.classList.remove('active');
-//                      favAdd.classList.add('inactive');
-//                      favAdd.src = "./assets/icon-fav.svg";
-                                  
-//                  }
-//              })
-//          };
-        
-//      }
-
-
-
-
-//insertar gifData en cada caja 
-
-let titleAndAuthorResult = (content) => {
-
-
-    let dataCtn = document.querySelectorAll('#resultCtn');
-
-
-
-    for (i = 0; i < dataCtn.length; i++) {
+let titleAndAuthor = (title, author, cajaGif) => {
+       
 
         let gifData = document.createElement('div');
-        gifData.classList.add('gifData');
-        gifData.id = 'gifData';
-        dataCtn[i].appendChild(gifData);
-        let title = document.createElement('h2');
-        title.classList.add('gifTitle');
-        title.id = 'gifTitle';
-        let titleCtn = content.data[i].title;
-        gifData.appendChild(title);
-        title.innerHTML = titleCtn;
-        let author = document.createElement('p');
-        author.id = 'gifAuthor';
-        gifData.appendChild(author);
-        let userName = content.data[i].username;
-        author.innerHTML = userName;
+        gifData.classList.add('gifData');        
+        cajaGif.appendChild(gifData);
+        let addTitle = document.createElement('p');
+        addTitle.classList.add('gifTitle');        
+        gifData.appendChild(addTitle);
+        addTitle.innerHTML = title;
+        let userName = document.createElement('p');        
+        gifData.appendChild(userName);        
+        userName.innerHTML = author;
     }
-}
+
 
 // Si hay una búsqueda previa, eliminar los resultados de la pantalla
 
--- scripts/trendings.js
+++ scripts/trendings.js
@@ -1,20 +1,59 @@
 let traer = ()=> {
     fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=15&offset=1&rating=g`)
     .then(res => res.json())
-    .then((gif)=>{
-         console.log(gif.data[1].images.downsized.url);
-         arrayGif(gif);   
-         titleAndAuthor(gif);
-         
-                      
-    })        
+    .then((gif)=>{           
+         arrayGif(gif);          
+         trendingArray(gif);                      
+    })     
  } 
         
        
- traer(); 
+ traer();  
  
- 
- 
+// crear array que contenga los gif trending resultado del fetch
+
+let trendingArray = (gif) => {
+   arrayTrendings.push(gif);
+   console.log('Estos son los Trendings!!')
+   console.log(arrayTrendings[0].data);
+}
+
+
+
+//consumir datos del array de trendings insertar resultados en DOM
+
+let arrayGif = (gif) =>{
+   for(var i = 0; i <= 14 ; i++){         
+   // url Imagen
+   let tdnGif = gif.data[i].images.downsized.url 
+   // Accedemos al nodo html
+   let tdnCtn = document.getElementById(`trending-ctn`); 
+   // crear elemento imagen
+   let createGif = document.createElement(`img`);
+   createGif.classList.add("gipho");
+   // asignar src imagen  = url.imagen
+   createGif.src= tdnGif;
+   /* createGif.id=`gipho${[i+1]}`; */
+   createGif.id=`gipho`;        
+   // creo padre para c/gif 
+   let crearDiv = document.createElement(`div`);
+   //insert gif en div 
+   crearDiv.appendChild(createGif);
+   crearDiv.classList.add("giphoBox")
+   crearDiv.id =`boxWithTitleToolsGif`;
+   //asignar hijo a nodo html
+   tdnCtn.appendChild(crearDiv);  
+   //capturar title+author para enviar como parameter a funcion
+   let titleGif = gif.data[i].title;
+   let authorGif = gif.data[i].username;
+   createTools(crearDiv);   
+   titleAndAuthor(titleGif, authorGif, crearDiv)             
+   }            
+}
+
+
+
+
      
 
     // accedo al objeto, recorro sus propiedades y accedo a una en particular
