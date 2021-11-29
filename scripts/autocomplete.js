f0505eccc138d12e98fba6a73048fd39285e6077
ezSan <ezequiel.sanchez.7391@gmail.com>
2021-11-29 18:23:29 -0300

    Doing closeSuggestions

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -25,31 +25,46 @@ keyPressInSearchInput()
 
 let displaySuggestionsToDOM = (suggestionsData) => {
 
-
-    console.log(suggestionsData)
     let mapSuggestionsArray = suggestionsData.map(displaySugg => {
 
         let autocompleteBox = document.getElementById('autocompleteSuggestionsBox');
 
         let suggestionsBox = document.createElement('div');
         suggestionsBox.classList.add('suggestionBox');
+        suggestionsBox.addEventListener('click', fetchSuggestionTerm =(e)=>{
+            let termSuggestion = e.currentTarget.childNodes[1].innerText;
+            console.log(termSuggestion);
+            let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=24&q=${termSuggestion}`
 
+            fetch(url)
+            .then(content => content.json())
+            .then(content=>{
+                displayResultsInDOM(content);
+                closeSuggestionsBox(autocompleteBox);
+                addTitle(termSuggestion);
+            })
 
-
+        })
+            
+            
+        
         let createSuggestion = document.createElement('p');
         createSuggestion.classList.add('suggestionP');
         let suggestionName = displaySugg.name;
         createSuggestion.innerText = suggestionName;
-        suggestionsBox.insertAdjacentElement('afterbegin', createSuggestion)
+        suggestionsBox.insertAdjacentElement('afterbegin', createSuggestion);
+        
 
         let lupita = document.createElement('img');
-        lupita.src = 'assets/icon-search-mod-noc.svg'
+        lupita.src = 'assets/icon-search-mod-noc.svg';
+        
 
         suggestionsBox.insertAdjacentElement('afterbegin', lupita)
         autocompleteBox.insertAdjacentElement('afterbegin', suggestionsBox);
 
 
     })
-}
+}   
 
 
+function closeSuggestionsBox(autocompleteBox){autocompleteBox.classList.add('none')}
