63af17f86c031e3bc6eaffc94a14cd8ed149b33f
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-01-02 11:31:18 -0300

    recorder OK, toDo getBlob & upload file

--- scripts/webcam.js
+++ scripts/webcam.js
@@ -1,35 +1,100 @@
-function webcamAccessAndStream(){
+
+
+
+
+
+function webcamAccessAndStream() {
   navigator.mediaDevices.getUserMedia({
-    audio:true,
-    video:true}
+    audio: false,
+    video: true
+  }
   )
-  .then(stream=>{
+    .then(stream => {
       let video = document.getElementById('videoBox');
-      console.log(stream);
-      console.log(video)
-
       video.srcObject = stream;
-
       video.play()
+      video.classList.remove('none');
+      let step2 = document.getElementById('step2');
+      step2.classList.add('newStep1');
+
+      let prevStep = document.getElementById('step1');
+      prevStep.classList.remove('newStep1');
+
+      let appearBox = document.getElementById('appearBox');
+      appearBox.classList.add('none');
+
+
+      recorder = new RecordRTCPromisesHandler(stream, {
+        type: 'gif',
+        mimeType: 'video/webm',
+        recorderType: GifRecorder,
+        frameRate: 1,
+        quality: 10,
+        width: 360,
+        hidden: 240,
+
+      });
+
+      createRecButton()
+      
+
+    })
+}
+
+
+
+
+
+
+newColor = () => {
+  //Change Color Dot
+  let n1 = document.getElementById(`step1`);
+  n1.classList.add('newStep1');
+  let insideText = document.getElementById('insideText');
+  insideText.classList.add('none');
+  let appearBox = document.getElementById('appearBox');
+  appearBox.classList.remove('none');
+  let comenzarBtn = document.getElementById('comenzar');
+  comenzarBtn.classList.add('none');
+  webcamAccessAndStream();
+  
+}
+
+
+function createRecButton() {
+  let btn = document.createElement('input');
+  btn.type = 'button';
+  btn.value = 'GRABAR';
+  btn.classList.add('inputWebcamAcces')
+  btn.classList.add('recButton');
+  btn.id = 'recButton'
+  let btnCtn = document.getElementById('camAccess');
+  btnCtn.appendChild(btn);
+  btn.addEventListener('click', rec => {
+    recorder.startRecording()
+    createStopButton()
   })
 }
 
 
 
-newColor = () =>{
-   //Change Color Dot
-   let n1 = document.getElementById(`step1`)
-   n1.style.backgroundColor = '#572EE5';
-   n1.style.color=`white`;
-   let hiddenBox = document.getElementById(`insideText`);
-   hiddenBox.style.display="none"; 
-   let appearBox = document.getElementById(`appearBox`);
-   appearBox.classList.remove(`hiddenBox`);
-   appearBox.classList.add('none')
+function createStopButton() {
+  let btnStop = document.createElement('input');
+  let btnRec = document.getElementById('recButton');
+  btnRec.classList.add('none')
+  btnStop.type = 'button';
+  btnStop.value = 'FINALIZAR';
+  btnStop.classList.add('inputWebcamAcces');
+  btnStop.classList.add('btnStopRec')
+  let btnCtn = document.getElementById('camAccess');
+  btnCtn.appendChild(btnStop);
+  btnStop.addEventListener('click', rec => {
+    recorder.stopRecording()
+    let blob = recorder.getBlob(x=> blob.then(console.log(x)));
+    console.log(blob)
+  }
+    
+    )}
+  
 
-   let comenzarBtn = document.getElementById('comenzar');
-   comenzarBtn.classList.add('none');
 
-   webcamAccessAndStream()
-
-}
