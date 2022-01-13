da717e59ff72bc3452bbf69c4d3e52a3e9da2090
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-01-13 19:42:03 -0300

    styling preview overlay gifo

--- scripts/webcam.js
+++ scripts/webcam.js
@@ -1,7 +1,6 @@
 
 
-
-
+/* request permission to access the media stream */
 
 function webcamAccessAndStream() {
   step1.classList.add('newStep');
@@ -12,26 +11,19 @@ function webcamAccessAndStream() {
     audio: false,
     video: true
   })
-    .then(stream => {      
+    .then(stream => {
       video.srcObject = stream;
       video.play();
-      
+
       comenzar.classList.add('none');
 
-      step1.classList.remove('newStep');      
-      step2.classList.add('newStep');    
-      slide2.classList.add('none');    
+      step1.classList.remove('newStep');
+      step2.classList.add('newStep');
+      slide2.classList.add('none');
       previewAndVideoBox.classList.remove('none');
       slideWithInstructions.classList.add('none');
       btnRec.classList.remove('none');
       
-
-      /* slide1.classList.add('none');
-      slide2.classList.remove('none'); */
-      
-      
-
-
       recorder = new RecordRTCPromisesHandler(stream, {
         type: 'gif',
         mimeType: 'video/webm',
@@ -42,68 +34,57 @@ function webcamAccessAndStream() {
         hidden: 240,
 
       });
-
-      
-
-
     })
 }
-
-comenzar.addEventListener('click' , webcamAccessAndStream );
-
-uploadGiphoButton.addEventListener('click' , postGipho =>{
-  uploadToGiphy(fileToUpload)
-})
-
-btnRec.addEventListener('click', rec => {
-    recorder.startRecording()
-    btnStop.classList.remove('none')
-    createStopButton()
-  })
-
-
-
-
-function createStopButton() {  
-  btnRec.classList.add('none');  
-  btnStop.addEventListener('click', stop => {
-    recorder.stopRecording();
-    let blobUrl =  recorder.getBlob();
-    blobUrl.then(blob=>{
-      let urlGiphoForUpload = URL.createObjectURL(blob)
-      fileToUpload = new FormData();
-      fileToUpload.append('file' , blob, "myGif.gif");
-      console.log(fileToUpload.get('file'));         
-      btnStop.classList.add('none');      
-      video.classList.add('none');      
-      uploadGiphoButton.classList.remove('none');
-      uploadGipho.classList.remove('none');      
-      uploadGipho.src = urlGiphoForUpload;
-      })    
-  })
-}
-
-
 
 
 //upload gipho endpoint
 
-const uploadToGiphy = async (fileGif) => { 
+const uploadToGiphy = async (fileGif) => {
   try {
-    let response = await fetch(urlEndpointUpload,{
+    let response = await fetch(urlEndpointUpload, {
       method: 'POST',
       body: fileGif,
     });
     let newGif = response.json();
-    newGif.then(newGif=>{
+    newGif.then(newGif => {
       let idGiphoUploaded = newGif.data.id;
       uploadedGiphosId.push(idGiphoUploaded);
       console.log(idGiphoUploaded);
     })
-    
 
-  } catch(err) {
+
+  } catch (err) {
     console.log(err)
   }
 }
+
+comenzar.addEventListener('click', webcamAccessAndStream);
+
+uploadGiphoButton.addEventListener('click', postGipho => {
+  uploadToGiphy(fileToUpload)
+})
+
+btnRec.addEventListener('click', rec => {
+  recorder.startRecording()
+  btnRec.classList.add('none')
+  btnStop.classList.remove('none');
+})
+
+btnStop.addEventListener('click', stop => {
+  recorder.stopRecording();
+  let blobUrl = recorder.getBlob();
+  blobUrl.then(blob => {
+    let urlGiphoForUpload = URL.createObjectURL(blob)
+    fileToUpload = new FormData();
+    fileToUpload.append('file', blob, "myGif.gif");
+    console.log(fileToUpload.get('file'));
+    btnStop.classList.add('none');
+    video.classList.add('none');
+    uploadGiphoButton.classList.remove('none');
+    uploadGipho.classList.remove('none');
+    uploadGipho.src = urlGiphoForUpload;
+  })
+})
+
 
