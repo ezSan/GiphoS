f4219c606d8efe90dacd9a81113e135aed2203bf
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-03-22 18:02:53 -0300

    DarkMode Styled

---
+++ scripts/variables.js
@@ -0,0 +1,80 @@
+//ApiKey giphy
+const apiKey = "2F7Zwq8U9gs2PssCkAKfR65Ned1IPdpb"; 
+
+
+//DarkMode
+const first = document.getElementById('first');
+const darkModeBtn = document.getElementById('darkMode');
+const headerCtn = document.getElementById('headerCtn');
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const trending = document.getElementById('trending');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const mainTitle = document.getElementById('mainTitle');
+const sliderTitleAndParagraph = document.getElementById('sliderTitleAndParagraph');
+/* Variables webcam page */
+const video = document.getElementById('videoBox');
+const previewAndVideoBox = document.getElementById('previewAndVideoBox');
+const slideWithInstructions = document.getElementById('slideWithInstructions');
+const uploadGipho = document.getElementById('uploadGipho');
+const uploadGiphoButton = document.getElementById('uploadGiphoButton');
+const btnStop = document.getElementById('btnStopRec');
+const btnRec = document.getElementById('recButton');
+const slide1 = document.getElementById('slide1');
+const slide2 = document.getElementById('slide2');
+const comenzar = document.getElementById('comenzar');
+const step1 = document.getElementById('step1');
+const step2 = document.getElementById('step2');
+const uploadingGifOverlay = document.getElementById('uploadingGifOverlay');
+const overlayBoxPending = document.getElementById('overlayBoxPending');
+const overlayBoxOk = document.getElementById('overlayBoxOk');
+const recordAgain = document.getElementById('recordAgain');
+/* gif file ready to upload whit api endpoint */
+let fileToUpload;
+/* display search results in DOM */
+const searchInput = document.querySelector('#search');
+const btnSearch = document.getElementById("btnSearch");
+const searchResultsContainer = document.getElementById('searchResultsContainer');
+const verMas = document.getElementById('verMas');
+/* variables DOM slider trending buttons*/
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const prevStepImg = document.getElementById('prevStepImg');
+const nextStepImg = document.getElementById('nextStepImg');
+const autocompleteBox = document.getElementById('autocompleteSuggestionsBox');
+/* trending Bar suggestions */
+const trendingSuggestionsBox = document.getElementById('trendingSuggestionsBox');
+const trendingBarSuggestionsBox = document.getElementById('trendingBarSuggestionsBox');/* endpoints giphy Api */
+const urlEndpointTrending = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=15&offset=1&rating=g`;
+const urlEndpointTermTrending = `https://api.giphy.com/v1/trending/searches?api_key=${apiKey}`;
+const urlEndpointUpload = `https://upload.giphy.com/v1/gifs?api_key=${apiKey}`;
+const urlEndpointSearch = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=24&q=`;
+
+
+
+
+/* mis giphos */
+
+const misGifosContainer = document.getElementById('misGifosContainer');
+const emptyMyGifos = document.getElementById('emptyMyGifos');
+
+// array provisorio que almacena resultados de busqueda
+let resultadosDeBusqueda = [];
+// Array que almacena resultados favoritos
+let favoritos = [];
+//array de id´s de favoritos
+let favId = [];
+//array para trendings 
+var arrayTrendings = [];
+//local storage
+let storageFavoritos = JSON.parse(localStorage.getItem('Favoritos'));
+
+//array of created gif id 
+
+let gifCreatedIds = [];
+
+
+let gifosIdsStorage = JSON.parse(localStorage.getItem('MyGifosIds'))
+/* let storageGifosIds = JSON.parse(gifosIdsStorage); */
+
